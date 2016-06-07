11e8177990a708e14be1ec293a573cee565b3c8a
memsharded <james@conan.io>
2016-06-08 02:06:38 +1000

    support for static-windows

--- test/conanfile.py
+++ test/conanfile.py
@@ -21,7 +21,7 @@ class DefaultNameConan(ConanFile):
 
     def build(self):
         cmake = CMake(self.settings)
-        self.run('cmake . %s' % cmake.command_line)
+        self.run('cmake %s %s' % (self.conanfile_directory, cmake.command_line))
         self.run("cmake --build . %s" % cmake.build_config)
 
     def imports(self):
@@ -29,4 +29,4 @@ class DefaultNameConan(ConanFile):
         self.copy(pattern="*.dylib", dst="bin", src="lib")
 
     def test(self):
-        self.run("cd bin && .%sexample ../OpenSans-Bold.ttf 'lasote!' > file.txt" % (os.sep))
+        self.run("cd bin && .%sexample ../../../OpenSans-Bold.ttf 'lasote!' > file.txt" % (os.sep))
